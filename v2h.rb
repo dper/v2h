bab87d311f8f0a875d3493b1b8d05405df1d6f9d
Douglas Perkins <webmaster@dperkins.org>
2014-01-30 18:34:02 +0900

    Improving script.

--- v2h.rb
+++ v2h.rb
@@ -7,11 +7,23 @@ $filename = "contacts.vcf"
 
 # Converts a single Vcard to HTML.
 def vcard_to_html card
-	html = "<div>"
+	html = '<div class="vcard">' + "\n"
 
-	html += card.name.fullname
+	html += '<span class="fn">' + card.name.fullname + '</span>' + "\n"
 
-	html += "</div>\n"
+	if card.telephones.size > 0
+		card.telephones.each do |tel|
+			html += '<div class="tel">' + tel + '</div>' + "\n"
+		end
+	end
+
+	if card.emails.size > 0
+		card.emails.each do |email|
+			html += '<a class="email" href="' + email + '">' + email + '</a>' + "\n"
+		end
+	end
+
+	html += '</div>' + "\n"
 	return html
 end
 
@@ -21,15 +33,15 @@ def vcf_to_html
 	s = File.read($filename)
 	cards = Vcard::Vcard.decode(s)
 
-	html = "<html>\n"
-	html += "<body>\n"
+	html = '<html>' + "\n"
+	html += '<body>' + "\n"
 
 	cards.each do |card|
 		html += vcard_to_html card
 	end
 
-	html += "</body>\n"
-	html += "</html>\n"
+	html += '</body>' + "\n"
+	html += '</html>' + "\n"
 
 	#TODO Write this to a file.
 	puts html
